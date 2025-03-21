473e90d8d2e9c2663a5ee850d8eaf05bc35b4a7f
saeed <saeed@gmail.com>
2025-03-21 14:43:24 +0330

    edit slider

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <header>header</header>
-      <div>slider one</div>
+      <div>my slider one</div>
       <div>slider two</div>
       <div>slider three</div>
       <footer>change footer</footer>
